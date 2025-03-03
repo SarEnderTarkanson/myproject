b4be71418fb2c8ae386c32c0e60f2852b3eb0432
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-03 10:48:55 +0100

    created our first basic modular component

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,46 @@
 <script>
-	let people = [
-		{ name: "Ender", beltColor: "black", age: 6, id: 1 },
-		{ name: "Sare", beltColor: "pink", age: 9, id: 2 },
-		{ name: "Makbule", beltColor: "black", age: 38, id: 3 },
-	];
+  import Modal from "./Modal.svelte";
+  let people = [
+    { name: "Ender", beltColor: "black", age: 6, id: 1 },
+    { name: "Sare", beltColor: "pink", age: 9, id: 2 },
+    { name: "Makbule", beltColor: "black", age: 38, id: 3 },
+  ];
 
-	const handleClick = (id) => {
-		people = people.filter((person) => person.id != id);
-	};
+  const handleClick = (id) => {
+    people = people.filter((person) => person.id != id);
+  };
 
-	let num = 5;
+  let num = 5;
 </script>
 
-<!-- {#if num > 20}
-	<p>Greater than 20</p>
-	{:else if num > 5 }
-	<p>Greater than 5</p>
-	{:else}
-	<p>Not greater than 5</p>
-{/if} -->
+<Modal />
 
 <main>
-	{#each people as person (person.id)}
-		<div>
-			<h4>{person.name}</h4>
-			{#if person.beltColor === "black"}
-			<p><strong>Master Ninja</strong></p>
-			{/if}
-			<p>{person.age} years old, {person.beltColor} belt.</p>
-			<button on:click={() => handleClick(person.id)}>delete</button>
-		</div>
-	{:else}
-		<p>There are no people to show...</p>
-	{/each}
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColor === "black"}
+        <p><strong>Master Ninja</strong></p>
+      {/if}
+      <p>{person.age} years old, {person.beltColor} belt.</p>
+      <button on:click={() => handleClick(person.id)}>delete</button>
+    </div>
+  {:else}
+    <p>There are no people to show...</p>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
