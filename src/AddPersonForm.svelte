5cdbf9291965777c778dd99a788868a571d57058
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-06 21:52:36 +0100

    added checkbox and select to the form component

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,16 +2,37 @@
     let name;
     let beltColor;
     let age;
+    // let fighting = false;
+    // let sneaking = false;
+    // let running = false;
+    let skills = [];
     const handleSubmit = () => {
-        console.log("name: ", name, "beltColor: ", beltColor, "age: ", age);
+        console.log(
+            "name: ",
+            name,
+            "beltColor: ",
+            beltColor,
+            "age: ",
+            age,
+            skills,
+        );
     };
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name} />
-    <input type="text" placeholder="belt color" bind:value={beltColor} />
     <input type="number" placeholder="age" bind:value={age} />
-    <br />
+    <label>Skills:</label>
+    <input type="checkbox" bind:group={skills} value="figthing" />Fighting<br />
+    <input type="checkbox" bind:group={skills} value="sneaking" />Sneaking<br />
+    <input type="checkbox" bind:group={skills} value="running" />Running<br />
+    <label>Belt Color</label>
+    <select bind:value={beltColor}>
+        <option value="Black">Black</option>
+        <option value="Orange">Orange</option>
+        <option value="Brown">Brown</option>
+        <option value="White">White</option>
+    </select>
     <button>Add Person</button>
 </form>
 
